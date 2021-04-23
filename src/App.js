eaed5a037e4c9339b1e8a4ce95fcecde456059a9
Yassin Elsir <yassinmelsir@gmail.com>
2021-04-23 13:35:57 -0500

    added blog template and restyled page to include linkedin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React from "react"
+import React, {useState} from "react"
 
-import { SiGithub, SiGmail, SiTwitter } from 'react-icons/si'
+import { SiGithub, SiGmail, SiTwitter, SiLinkedin } from 'react-icons/si'
 
-const ContactForm = () => {
+import { GiQuillInk } from 'react-icons/gi'
+
+import { IoMdClose } from "react-icons/io";
+
+const ContactForm = ({className}) => {
   return (
     <form
       className=""
       name="contact-home-page"
       data-netlify="true"
     >   
-        <div className='grid grid-flow-row place-items-center mt-20 gap-1'>
+        <div className={'grid grid-flow-row place-items-center gap-1 ' + className}>
           <input
-            className="w-3/4 bg-gray-200 rounded-full text-xl mt-5"
+            className="w-3/4 bg-gray-100 rounded-full text-xl mt-5"
             name="name"
             type="text"
             placeholder="Name"
           />
           <input
-            className="w-3/4 bg-gray-200 text-xl rounded-full"
+            className="w-3/4 bg-gray-100 text-xl rounded-full"
             name="email"
             type="email"
             placeholder="Email"
@@ -33,27 +37,61 @@ const ContactForm = () => {
     </form>
   )
 }
-
-const Main = () => {
-  const buttonClass = 'hover:bg-gray-100 rounded-full w-40 grid grid-cols-1 place-items-center h-40 absolute z-10'
-  return ( 
-    <div className='grid grid-cols-1 place-items-center h-screen w-screen relative'>
-        <div className='grid grid-flow-col place-items-center relative'>
-          <div className='right-20 w-40 h-60 rounded-full hover:opacity-50 bg-gray-200 opacity-0 absolute z-20 -top-20 grid grid-flow-row place-items-center'>
-            <ContactForm />
-          </div>
-          <button className={buttonClass + ' left-20'}><a target="_blank" rel="noopener noreferrer" href='https://github.com/yassinmelsir'><SiGithub className='h-20 w-20'/></a></button>
-          <button className={buttonClass + ' right-20'}><SiGmail className='h-20 w-20' /></button>
-          <button className={buttonClass} ><a target="_blank" rel="noopener noreferrer" href='https://twitter.com/yassinelsir1'><SiTwitter className='h-20 w-20' /></a></button>
-        </div>
-        
+const Blog = ({props}) => {
+  const { route, setRoute } = props
+  return (
+    <div className='h-screen w-screen grid grid-flow-col place-items-center relative'>
+      <button onClick={()=> setRoute('main')} className='absolute h-10 w-10 top-0 right-0'><IoMdClose className='w-full h-full' /></button>
+      <p>Hello World!</p>
     </div>
+    
+  )
+}
+
+const Main = ({props}) => {
+  const buttonClass = 'hover:bg-gray-100 rounded-full w-40 grid grid-cols-1 place-items-center '
+  const containerPosition = ' top-20 xl:top-halfh absolute'
+  const { route, setRoute } = props
+  return ( 
+    
+    
+        <div className='relative'>
+          <div className={'z-10 grid grid-cols-3 xl:grid-flow-col gap-x-10 xl:gap-x-0 place-items-center ' + containerPosition + ' left-10 xl:left-tw '} >
+            <button className={buttonClass + 'h-40 '}><a target="_blank" rel="noopener noreferrer" href='https://github.com/yassinmelsir'><SiGithub className='h-20 w-20'/></a></button>
+            
+            <button className={buttonClass + 'h-40 '} ><a target="_blank" rel="noopener noreferrer" href='https://www.linkedin.com/in/yassin-elsir-2ab05b174/'><SiLinkedin className='h-20 w-20' /></a></button>
+            <button className={buttonClass + 'h-40 '} ><a target="_blank" rel="noopener noreferrer" href='https://twitter.com/yassinelsir1'><SiTwitter className='h-20 w-20' /></a></button>
+            <button className={buttonClass + 'h-40 ' + ' ml-40 xl:ml-0'} ><GiQuillInk onClick={()=> setRoute('blog')} className='h-20 w-20' /></button>
+            <button className={buttonClass + 'h-40 ' + ' ml-40 xl:ml-0 relative'}>
+              
+              
+                
+                <SiGmail className='h-20 w-20 absolute z-20' />
+                <div className='hover:opacity-100 rounded-full top-5 hover:bg-gray-100 opacity-0 absolute z-30 w-40 gap-0 h-60 grid grid-flow-row place-items-center '>
+                  <SiGmail className='h-20 w-20 mt-4' />
+                  <ContactForm className='-mt-10' />
+                </div>
+                
+                
+              
+              
+              </button>
+          </div>
+          
+        </div>
+    
+    
   )
 }
 
 const App = () => {
+    const [route, setRoute] = useState('main')
+    const props = { route, setRoute }
     return ( 
-      <Main />
+      <>
+        {route === 'blog' && <Blog props={props} />}
+        {route === 'main' && <Main props={props} />}
+      </>
     )
 }
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      inset: {
+        'qh': '25vh',
+        'qw': '25vw',
+        'th': '30vh',
+        'tw': '30vw',
+        'tfh': '35vh',
+        'tfw': '35vw',
+        'halfh': '50vh',
+        'halfw': '50vw',
+       }
     },
   },
   variants: {
